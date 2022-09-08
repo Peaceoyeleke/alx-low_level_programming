00af19c36bc778275316730403ff608f12d242f7
peaceoyeleke <oyelekept@gmail.com>
2022-09-08 15:41:56 -0700

    script to print to the standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,7 +3,7 @@
  * main - Entry point
  * Return: Always 1 (Success)
  */
-int main(void);
+int main(void)
 {
 write(STDERR_FILENO, "and that piece of arti useful - Dora Korpar, 2015-10-19 \n");
 return (1);
