4c64e8d60f14453cdc6a093b9db81ee45690cde9
peaceoyeleke <oyelekept@gmail.com>
2022-09-08 13:24:01 -0700

    my first c program using the puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * 
- * return = Always 0 (Success)
+ *
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-puts("\" Programming is like buiding a multilingual puzzle");
+puts("\"Programming is like buiding a multilingual puzzle");
 
 return (0);
-}   
+}
