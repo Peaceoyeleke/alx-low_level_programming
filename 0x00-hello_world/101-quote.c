bc5be71167e27e753bc7c8a24a12a0b8656e77a8
peaceoyeleke <oyelekept@gmail.com>
2022-09-08 15:30:57 -0700

    script that prints to the standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 1 (Success)
+ */
+int main(void);
+{
+dprintf(STDERR_FILENO, "and that piece of arti useful - Dora Korpar, 2015-10-19 \n")
+return (1)
+}
