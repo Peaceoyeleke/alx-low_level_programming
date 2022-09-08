a72d8dc8de86254e51a2f4775ccad4cd71c3edf9
peaceoyeleke <oyelekept@gmail.com>
2022-09-08 16:43:39 -0700

    script to print to the standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,8 @@
 #include<unistd.h>
 
 /**
- * main - Prints out the last part of a quote in the standard error .
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",followed by a new line, to standard error.
+ *
  * Return: Always 1 (Success)
  */
 int main(void)
