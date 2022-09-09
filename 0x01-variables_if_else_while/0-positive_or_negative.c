de1ff6c6245eee1ca782faacc584c95d9ffcec0d
peaceoyeleke <oyelekept@gmail.com>
2022-09-09 04:57:13 -0700

    program that prints positive or negative numbers

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -18,10 +18,10 @@ printf("%d is positive/n", n);
 }
 else if(0 == 0)
 {
-printf("%D is zero/n", n);
+printf("%d is zero/n", n);
 }
 else {
-printf("%d is negative/n", n)
+printf("%d is negative/n", n);
 }
 return (0);
 }
