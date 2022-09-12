51cd9ef808957fe8629938684c9f7df77a85341d
peaceoyeleke <oyelekept@gmail.com>
2022-09-12 11:28:28 -0700

    printing to standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<unistd.h>
 
 /**
  * main - Prints the standard error sandwitched around the standard output
@@ -7,7 +8,11 @@
  **/
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19 stderr\n", 59);
+int 1;
+
+1 = sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19 \n", 1);
 
 return (1);
 }
