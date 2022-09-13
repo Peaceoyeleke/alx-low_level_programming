a3e96147bfff6ae0b7d81bf2fec5f463fdec1f1c
peaceoyeleke <oyelekept@gmail.com>
2022-09-13 05:54:05 -0700

    using putchar to print

---
+++ 0-putchar.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+/**
+ * Main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+/*
+ * a program that prints _putchar followed by a new line
+ */
+{
+
+{
+putchar('_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
+}
+
+{
+putchar('\n');
+}
+return (0);
+}
