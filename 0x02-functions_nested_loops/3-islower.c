c4004fa504b151bc95933aba1a4e07d986c1a991
peaceoyeleke <oyelekept@gmail.com>
2022-09-13 12:47:45 -0700

    checking i c is an alphabet

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lower case
+ *
+ * Return: 0 or 1
+ */
+
+int _islower(int c)
+{
+int c;
+if (c = 'a' && c <= 'z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
