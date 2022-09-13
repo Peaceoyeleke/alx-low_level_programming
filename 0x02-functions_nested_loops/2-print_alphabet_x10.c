57930396ad36ca0572664decd2447152f90f0ed7
peaceoyeleke <oyelekept@gmail.com>
2022-09-13 12:16:10 -0700

    printing the alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main"
+
+/**
+ * print_alphabet_x10 - prints the alphabet 10 times
+ *
+ * Return: Nothing
+ */
+
+void print_alphabet_x10(void)
+{
+int x;
+int count = 0;
+
+while (count++ <= 9)
+{
+for (x = 'a'; x <= 'z'; x++)
+{
+putchar(x);
+}
+{
+putchar('\n');
+}
+}
+}
