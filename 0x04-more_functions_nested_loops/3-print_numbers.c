427bb1b4d586cb6869794ce5c5dc0efeaa5696b2
peaceoyeleke <oyelekept@gmail.com>
2022-09-15 14:54:22 -0700

    a function that prints the numbers, from 0 to 9, followed by a new line

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,17 +1,18 @@
 #include "main.h"
 
 /**
- * print_numbers - a function that prints the numbers, from 0 to 9, followed by a new line
+ * print_numbers - a function that prints the numbers, from 0 to 9
  *
  * Return: nothing
  */
 
 void print_numbers(void)
 {
-int n;
-while (n >= 0; n <= 9; n++)
+int n = 0;
+while (n <= 9)
 {
-_putchar('n');
+_putchar(n + '0');
+n++;
 }
 _putchar('\n');
 }
