32dea01b56015e1bdde3c3a83093f9bf45f4978f
peaceoyeleke <oyelekept@gmail.com>
2022-09-15 07:47:19 -0700

    program that comments out

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - causes an indefinite loop
+ * main - causes an infinite loop
  * Return: 0
  */
 
@@ -9,7 +9,7 @@ int main(void)
 {
 	int i;
 
-	printf("Indefinite loop incoming :(\n");
+	printf("Infinite loop incoming :(\n");
 
 	i = 0;
 /*
