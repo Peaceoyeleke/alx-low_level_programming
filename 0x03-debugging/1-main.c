808c602c73da68007a6a4c4cee5de58addac2896
peaceoyeleke <oyelekept@gmail.com>
2022-09-15 06:38:03 -0700

    program that comments out

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,18 +7,18 @@
 
 int main(void)
 {
-int i;
+	int i;
 
-printf("Indefinite loop incoming :(\n");
+	printf("Indefinite loop incoming :(\n");
 
-i = 0;
+	i = 0;
 /*
 *while (i < 10}
 *{
 *putchar(i);
 *}
 */
-printf("Infinite loop avoided! \\o/\n");
+	printf("Infinite loop avoided! \\o/\n");
 
-return (0);
+	return (0);
 }
