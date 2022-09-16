a956cc08242b9ae37ae3f25c4eb39aad8c65d7cd
peaceoyeleke <oyelekept@gmail.com>
2022-09-16 14:02:29 -0700

    program that prints the larest prime factor

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -2,7 +2,7 @@
 #include "stdio.h"
 
 /**
- * main prints the largest prime factor of the number 612852475143
+ * main - prints the largest prime factor of the number 612852475143
  *
  * Return: 0
  */
@@ -19,15 +19,15 @@ num = 612852475143;
 while (num % 2 == 0)
 {
 max = 2;
-n /= 2;
+num /= 2;
 }
 
-for (i = 3; i <= num / 2; i + 2)
+for (i = 3; i <= num / 2; i += 2)
 {
-while (n % i == 0)
+while (num % i == 0)
 {
 max = i;
-n /= i;
+num /= i;
 }
 }
 if (num > 2)
