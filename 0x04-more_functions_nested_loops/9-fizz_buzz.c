5fdd5f473b68ab6386cbf1ba9713ce1911092fb4
peaceoyeleke <oyelekept@gmail.com>
2022-09-15 18:03:20 -0700

    fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,22 +10,25 @@
 int main(void)
 {
 int n;
-for (n = 1; n <= 100; n++)
-{
-_putchar('n');
-_putchar(' ');
-}
-_putchar('\n');
-if (n % 3 == 0)
-{
+while (n++ < 100)
+if ((n % 3 == 0) && (n % 5 == 0))
+printf("FizzBuzz ");
+
+else if (n % 3 == 0)
 printf("Fizz ");
-}
+
 else if (n % 5 == 0)
 {
+if (n != 100)
 printf("Buzz ");
+
+else
+printf("Buzz");
 }
-else if ((n % 3 == 0) && (n % 5 == 0))
-{
-printf("FizzBuzz ");
-}
+else
+printf("%d ", n);
+
+printf("\n");
+
+return (0);
 }
