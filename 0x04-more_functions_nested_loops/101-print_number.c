c4790e915ff3243f2322710900d2472f98974e29
peaceoyeleke <oyelekept@gmail.com>
2022-09-16 15:04:16 -0700

    prints an integer

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -10,21 +10,15 @@
 
 void print_number(int n)
 {
-unsigned int num = n;
-if (num < 0);
+if (n < 0);
 {
 _putchar('-');
-num = -n;
+n = n * -1;
 }
-else
+if ((n / 10) > 0)
 {
-num = n;
+print_number(n / 10);
 }
-
-if ((num / 10) > 0)
-{
-print_number(num / 10);
-}
-_putchar((num % 10) + '0');
+_putchar((n % 10) + '0');
 }
 
