2639b7a27ca6a779e24e4e9e96f156e5cc5c401f
peaceoyeleke <oyelekept@gmail.com>
2022-09-19 11:35:43 -0700

    prints a string

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -4,17 +4,15 @@
  * _puts - function that prints a string
  * @str: pointer to the  string
  *
- * Description: prints a string
- * On success: return the number of characters printed
+ * Return: void
  */
 
 void _puts(char *str)
 {
-int i;
+	int i;
 
-for (i = 0; str[i] != '\0'; i++)
-{
-_putchar(str[i]);
-}
-_putchar('\n');
+	for (i = 0; str[i] != '\0'; i++)
+		_putchar(str[i]);
+
+	_putchar('\n');
 }
