6584fe4042ac5ece10bc90995cfec94a416fca00
peaceoyeleke <oyelekept@gmail.com>
2022-09-19 07:07:45 -0700

    header file

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - a function that takes a pointer
+ * @n: parameter
+ * Return: nothing
+ */
+
+void reset_to_98(int *n)
+{
+int *n;
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,24 @@
+#ifdef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+
+/**
+ * main - header file for prototypes
+ *
+ * void prototypes (void)
+ * int prototypes (int)
+ */
+
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+int _putchar(char c);
+void print_rev(char *s);
+void _puts(char *str);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+#endif /*MAIN_H*/
