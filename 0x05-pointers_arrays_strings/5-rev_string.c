e2a7de0f56e4d9538e3cd571ddb4af0097d04724
peaceoyeleke <oyelekept@gmail.com>
2022-09-19 13:20:45 -0700

    reverses a string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -12,16 +12,16 @@ void rev_string(char *s)
 	int i, len, len1;
 
 	len = 0;
-	len1 =0;
+	len1 = 0;
 
 	while (s[len] != '\0')
 	{
 		len++;
 	}
 
-	len1 = len -1;
+	len1 = len - 1;
 
-	for (i = 0; i < len / 3; i++)
+	for (i = 0; i < len / 2; i++)
 	{
 		tmp = s[i];
 		s[i] = s[len1];
