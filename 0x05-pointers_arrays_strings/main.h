79971f9d9de562af8d7f239c981daf1410d915b2
peaceoyeleke <oyelekept@gmail.com>
2022-09-19 09:31:12 -0700

    header file

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -9,11 +9,10 @@
  * int prototypes(int)
  * char prototypes(char)
  */
-
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
-int _putchar(char c);
 void print_rev(char *s);
 void _puts(char *str);
 void rev_string(char *s);
