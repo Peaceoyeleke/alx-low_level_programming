75f76f79218e5c97fff856a94c182ea9e9fb8ee7
peaceoyeleke <oyelekept@gmail.com>
2022-09-23 12:58:32 -0700

    changes line of code

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * infinite_add - a function that adds two numbers.
+ * @n1: first number
+ * @n2: second number
+ * @r: bufffer that stores result
+ * @size_r: bufer size
+ * Return: sum
+ */
+
+char *infinite_add(char *n1, chr *n2, char *, int size_r)
+{
+	int i, j;
+
+	for (i = 
