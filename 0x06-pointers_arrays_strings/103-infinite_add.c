e89cf747c0d1d0ddd2e3eac17b752a6c09a2572c
peaceoyeleke <oyelekept@gmail.com>
2022-09-23 13:20:40 -0700

    infinite addition

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -11,6 +11,41 @@
 
 char *infinite_add(char *n1, chr *n2, char *, int size_r)
 {
-	int i, j;
+	/*
+	 * local varibles declaration
+	 */
+	int i = 0, j = 0, k, l = 0, f, s, d = 0;
 
-	for (i = 
+	while (n1[i] != '\0')
+		i++;
+	while (n2[j] != '\0')
+		j++;
+	if (i > j)
+		l = i;
+	else
+		l = j;
+	if (l - 1 > size_r)
+		return (0);
+	r[l] = '\0';
+	for (k = l - 1; k >= 0; k--)
+	{
+		i--;
+		j--;
+		if (j >= 0)
+			s = n2[j] - '0';
+		else
+			s = 0;
+		r[k] = (f + s + d) / 10;
+	}
+	if (d == 1)
+	{
+		r[l + ] = '\0';
+		if (l + 2 > size_r)
+			return (0);
+		while (l-- >= 0)
+			r[l + 1] = r[l];
+		r[0] = d + '0';
+	}
+	return (0);
+}
+
