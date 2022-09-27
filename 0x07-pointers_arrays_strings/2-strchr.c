2df17563d479459c748a16bc158e9cdd3d5485d0
peaceoyeleke <oyelekept@gmail.com>
2022-09-27 06:10:58 -0700

    header file

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,10 +11,10 @@ char *_strchr(char *s, char c)
 {
 	while (*s)
 	{
-       if (*s != c)
+		if (*s != c)
 		s++;
-       else
-	       return (s);
+		else
+			return (s);
 	}
 	if (c == '\0')
 		return (s);
