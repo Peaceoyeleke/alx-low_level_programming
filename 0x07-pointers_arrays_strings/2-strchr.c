904df66c998921ae1cfea3c2867b97ef3613fe22
peaceoyeleke <oyelekept@gmail.com>
2022-09-27 05:59:13 -0700

    locates a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a charcter in a string
+ * @s: string
+ * @c: character
+ * Return: c
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s)
+	{
+       if (*s != c)
+		s++;
+       else
+	       return (s);
+	}
+	if (c == '\0')
+		return (s);
+	return (NULL);
+}
