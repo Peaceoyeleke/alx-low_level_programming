4d54f6868df0cc1813725c945f45c60805682578
peaceoyeleke <oyelekept@gmail.com>
2022-09-27 08:25:55 -0700

    locates a substrin

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -21,7 +21,7 @@ char *_strstr(char *haystack, char *needle)
 	if (haystack[i] == needle[i])
 	{
 		do {
-			if (needle[i = 1] == '\0')
+			if (needle[i + 1] == '\0')
 				return (haystack);
 
 			i++;
