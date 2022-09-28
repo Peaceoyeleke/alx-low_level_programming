f824ed5bd62878f42e330ad3da60e3384e5a7d23
peaceoyeleke <oyelekept@gmail.com>
2022-09-28 11:52:19 -0700

    compadere strings

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * wildcmpp - compares two string
+ * @s1: string
+ * @s2: string
+ * Return: 1 if strings are identical else 0
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	if (s1 == '\0')
+	{
+		if (s2 != '\0' && s2 == '*')
+		{
+			return (wildcm(s1, s2 + 1));
+		}
+		return (*s2 == '\0');
+	}
+	if (*s2 == '*')
+	{
+		return (wildcmp(s1 +1,s2) || wildcmp(s1, s2 + 1));
+	}
+	else if (*s1 == s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	return (0);
+}
