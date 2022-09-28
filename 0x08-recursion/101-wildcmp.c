d9e441789e8d9c37d9193f5816541c626d7866d4
peaceoyeleke <oyelekept@gmail.com>
2022-09-28 11:59:55 -0700

    compadere strings

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * wildcmpp - compares two string
+ * wildcmp - compares two string
  * @s1: string
  * @s2: string
  * Return: 1 if strings are identical else 0
@@ -19,7 +19,7 @@ int wildcmp(char *s1, char *s2)
 	}
 	if (*s2 == '*')
 	{
-		return (wildcmp(s1 +1,s2) || wildcmp(s1, s2 + 1));
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
 	}
 	else if (*s1 == *s2)
 	{
