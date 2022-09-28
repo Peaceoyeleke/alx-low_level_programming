34e6f79806359b919effcf68a58d25267f083780
peaceoyeleke <oyelekept@gmail.com>
2022-09-28 06:58:09 -0700

    prints a string in the reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints a string in the reverse
+ * @s: string
+ */
+
+void _print_rev_recursion(*s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i--)
+	{
+		_print_rev_recursion(s[i] + 1);
+		_putchar(*s);
+	}
+
