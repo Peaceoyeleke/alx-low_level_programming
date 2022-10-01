5a415753ad5a7ddbea54e6b45913303fd388122d
peaceoyeleke <oyelekept@gmail.com>
2022-09-30 21:05:10 -0700

    prints the number o arguements passed into it

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -8,9 +8,9 @@
  * Return: 0
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char __attribute__((__unused__)) *argv[])
 {
-	printf("%d/n", argc);
+	printf("%d\n", argc);
 
 	return (0);
 }
