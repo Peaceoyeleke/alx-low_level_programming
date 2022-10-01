a1f5096a97a209de1826268704d13521a91c0b04
peaceoyeleke <oyelekept@gmail.com>
2022-09-30 20:39:08 -0700

    prints a programs name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints a programs name
+ * @argc: number of arguements
+ * @argv: array of strings
+ * Return: 0
+ */
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return(0);
+}
