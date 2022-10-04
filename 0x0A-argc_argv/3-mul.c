bd1b7ac6b92002ff211b68c383129afb9ebde30c
peaceoyeleke <oyelekept@gmail.com>
2022-10-04 01:52:11 -0700

    multilies two numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,27 +1,27 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
 
 /**
- * main - multiplies two numbers
- * @argc: number of arguements
- * @argv: array of strings
+ * main - prints multilication
+ * @argc: number of arguemnts
+ * @argv: array of arguments
  * Return: 0
  */
 
 int main(int argc, char *argv[])
 {
-int i, j, mul;
+	int i, j, mul;
 
-if (argc <= 2)
-{
-	printf("Error\n");
-return (1);
-}
 
-i = atoi(argv[1]);
-j = atoi(argv[2]);
-mul = i * j;
+	if (argc <= 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	i = atoi(argv[1]);
+	j = atoi(argv[2]);
+	mul = i * j;
 
-printf("%d\n", mul);
-return (0);
+	printf("%d\n", mul);
+	return (0);
 }
