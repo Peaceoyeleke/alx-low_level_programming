b1d22f91c1746f051c35aac9930c9b2c3cd4ab60
peaceoyeleke <oyelekept@gmail.com>
2022-10-13 16:56:17 -0700

    prints strings

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -15,9 +15,7 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int i;
 
 	if (n == 0)
-	{
 		return (0);
-	}
 
 	 va_start(num, n);
 	 
@@ -25,6 +23,7 @@ int sum_them_all(const unsigned int n, ...)
 			sum += va_arg(num, int);
 
 	va_end(num);
+
 	return (sum);
 
 }
