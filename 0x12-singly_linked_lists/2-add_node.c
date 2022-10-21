a1adb13e9f9295b0186ef7d858c0c8ce25f6dc31
peaceoyeleke <oyelekept@gmail.com>
2022-10-21 04:04:30 -0700

    adds a new node at the beginning of a list_t list

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -3,8 +3,27 @@
 /**
  * *add_node - adds a node to the beginning of the list
  * @str: string
- * @first: irst list
- * Return: first
+ *
+ * @head: first list
+ * Return: head
  */
 
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+	size_t n;
 
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->str = strdup(str);
+
+	for (n = 0; str[n]; n++)
+	{
+		new->len = n;
+		new->next = *head;
+		head = new;
+	}
+	return (*head);
+}
